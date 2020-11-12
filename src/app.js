b337d0bfbed3f63f0bf151e44703724bede3dfeb
lavanyasingh94 <lavanya.singh@brightlifecare.com>
2020-11-12 18:21:42 +0530

    complete routing

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch} from 'react-router-dom';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import {Home, Browse, Signin, Signup } from './pages';
+import * as ROUTES from './constants/routes';
+
 
 export default function App() {
   return (
     <Router>
-      <Route exact path = "/signin">
-        <JumbotronContainer/>
-        <FaqsContainer/>
-        <FooterContainer/>
+      <Route exact path = {ROUTES.SIGN_IN}>
+        <Signin/>
+      </Route>
+      <Route exact path = {ROUTES.SIGN_UP}>
+        <Signup/>
+      </Route>
+      <Route exact path = {ROUTES.BROWSE}>
+        <Browse/>
+      </Route>
+      <Route exact path = {ROUTES.HOME}>
+      <Home/>
       </Route>
     </Router>
   );
