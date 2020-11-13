60a6ea86fbf41c79eee8da0b2f4632e136d5bb20
lavanyasingh94 <lavanya.singh@brightlifecare.com>
2020-11-13 17:48:27 +0530

    add header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
+import {OptForm} from '../components';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
@@ -6,6 +8,15 @@ import { FaqsContainer } from '../containers/faqs';
 export default function Home(){
     return (
         <>
+        <HeaderContainer>
+            <OptForm>
+                    <OptForm.Input placeholder = "Email Address"/>
+                    <OptForm.Button>Try It Now</OptForm.Button>
+                    <OptForm.Break/>
+                    <OptForm.Text>Ready To Watch?Enter your email to create or restart your membership
+                    </OptForm.Text>
+            </OptForm>
+        </HeaderContainer>
         <JumbotronContainer/>
         <FaqsContainer/>
         <FooterContainer/>
